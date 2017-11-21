76634e73fb829fbbef02032c6865f76e2982605c
孟伟 <772333621@qq.com>
2017-11-21 17:57:55 +0800

    格式化文本

--- src/app.vue
+++ src/app.vue
@@ -5,27 +5,21 @@
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                theme: this.$store.state.theme
-            };
-        },
-        mounted () {
-
-        },
-        beforeDestroy () {
-
-        },
-        methods: {
-
-        }
+export default {
+  data() {
+    return {
+      theme: this.$store.state.theme
     };
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+};
 </script>
 
 <style>
-.app-main{
-    width: 100%;
-    height: 100%;
+.app-main {
+  width: 100%;
+  height: 100%;
 }
 </style>
